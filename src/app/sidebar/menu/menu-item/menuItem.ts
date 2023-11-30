536c29b93a66d7615a2da8b168e70d0029cfb50c
azizHr <azizharkati20@gmail.com>
2023-11-30 14:55:03 +0100

    create menuItem class {itemText, itemIcon} and create an array of this type to store menu items text and icon

---
+++ src/app/sidebar/menu/menu-item/menuItem.ts
@@ -0,0 +1,9 @@
+export class MenuItem {
+  text: string;
+  icon: string;
+
+  constructor(text: string, icon: string) {
+    this.text= text;
+    this.icon = icon;
+  }
+}
--- src/app/sidebar/menu/menu.component.ts
+++ src/app/sidebar/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {MenuItem} from "./menu-item/menuItem";
 
 @Component({
   selector: 'app-menu',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
+
+  menuItems: MenuItem[] = [
+    new MenuItem('Dashboard', 'fa-solid fa-bars'),
+    new MenuItem('Subjects', 'fa-regular fa-file'),
+    new MenuItem('Levels', 'fa-regular fa-circle-up'),
+    new MenuItem('Questions', 'fa-regular fa-circle-question'),
+    new MenuItem('Medias', 'fa-regular fa-circle-play'),
+    new MenuItem('Responses', 'fa-regular fa-comment'),
+    new MenuItem('Validations', 'fa-regular fa-sun'),
+  ];
 
 }
