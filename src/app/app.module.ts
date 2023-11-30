18a859a412c1e6068055fbaf405ac6b3a33f734f
azizHr <azizharkati20@gmail.com>
2023-11-30 13:24:23 +0100

    create a menu-item component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { LogoComponent } from './sidebar/logo/logo.component';
 import { MenuComponent } from './sidebar/menu/menu.component';
+import { MenuItemComponent } from './sidebar/menu/menu-item/menu-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
     LogoComponent,
-    MenuComponent
+    MenuComponent,
+    MenuItemComponent
   ],
   imports: [
     BrowserModule
