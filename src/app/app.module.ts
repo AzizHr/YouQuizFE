73b941285c99b2acf117b7f08185aded5fa9e321
azizHr <azizharkati20@gmail.com>
2023-11-30 13:21:05 +0100

    create a logo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { LogoComponent } from './sidebar/logo/logo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    SidebarComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule
