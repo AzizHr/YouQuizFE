d8ea10a2a2a8bd8e599515ba90dac16db080f54c
azizHr <azizharkati20@gmail.com>
2023-12-01 17:30:24 +0100

    create findAll function [get all subjects]

--- src/app/services/subject.service.ts
+++ src/app/services/subject.service.ts
@@ -1,10 +1,17 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SubjectService {
 
-  constructor() { }
+  private api: string = 'localhost:8080/api/subjects';
+
+  constructor(private http: HttpClient) {}
+
+  findAll(): Observable<any> {
+    return this.http.get<any>(this.api);
+  }
 }
